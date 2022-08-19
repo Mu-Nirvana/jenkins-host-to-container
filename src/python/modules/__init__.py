65c306a895ff2c25fa63ff5ec20e1a42fb163eaa
bczemp <ben@parigan.com>
2022-08-19 13:42:14 -0400

    Created modules structure

---
+++ src/python/modules/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["value_sub"]
--- src/python/modules/value_sub.py
+++ src/python/modules/value_sub.py
@@ -2,7 +2,7 @@
 import re
 import yaml
 
-SUB_PATTERN = "\$\{\w+\}"
+SUB_PATTERN = "\$\{[\w.]+\}"
 
 def get_file(filename):
     with open(filename) as file:
@@ -13,16 +13,16 @@ def getYaml(yamlFile):
   with open(yamlFile) as file:
     return yaml.safe_load(file)
     
-def find_substrings(file, pattern):
+#
+def find_keys(file, pattern):
     return re.findall(pattern, file)
 
-def replace_substrings(file, substrings):
-    for substring in substrings:
-       file.replace(resolve_key(substring)) 
+def replace_keys(file, keys, key_values):
+    for key in keys:
+       file = file.replace(key, resolve_key(key.strip('${}'), key_values)) 
     return file
 
 def resolve_key(key, key_values):
     if '.' in key:
-        return resolve_key(key_values[key.strip("${}")
-
-
+        return resolve_key(key[key.index('.')+1:], key_values[key[:key.index('.')]])
+    else: return key_values[key]
